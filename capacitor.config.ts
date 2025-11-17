af480f7b6663ba5129981ac372e6be2389a9d264
corelca54 <118087457+corelca54@users.noreply.github.com>
2025-11-17 07:10:23 +0000

    Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.juegomania.app',
+  appName: 'juegomania',
+  webDir: 'out'
+};
+
+export default config;
